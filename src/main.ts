0cbc329aa6b1b0695eb36d0290111e445676f4ef
evsikoff <gsmevsikov@gmail.com>
2025-09-08 19:46:46 +0000

    CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Включаем CORS
+  app.enableCors({
+    origin: true, // Разрешаем запросы с любых доменов (для разработки)
+    // Для продакшена лучше указать конкретные домены:
+    // origin: ['http://localhost:3000', 'http://localhost:3001', 'https://yourdomain.com'],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // Если нужны cookies/authentication
+  });
+
   await app.listen(3000);
 }
 bootstrap();
