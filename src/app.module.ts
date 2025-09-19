bbb9cc3425d6a7ac36f0d0d3fc71ce2002b3936a
Evsikoff <gsmevsikov@gmail.com>
2025-09-19 20:53:14 +0300

    Configure SSL for database connection and fix module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { MonstertypesController } from "./monstertypes.controller";
+import { MonstertypesService } from "./monstertypes.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersAndMonsters } from "./entities/users-and-monsters.entity";
 import { CompetitionsInstancesMonsters } from "./entities/competitions-instances-monsters.entity";
@@ -10,22 +12,16 @@ import { join } from "path";
   imports: [
     TypeOrmModule.forRoot({
       type: "postgres",
-      url: "postgresql://user1:8208150aAa!@rc1a-cd1vstc3dofdrqa0.mdb.yandexcloud.net:6432/db1",
+      url: process.env.DATABASE_URL || "postgresql://user1:8208150aAa!@rc1a-cd1vstc3dofdrqa0.mdb.yandexcloud.net:6432/db1",
       entities: [UsersAndMonsters, CompetitionsInstancesMonsters],
       synchronize: false, // Для разработки можно установить true
       ssl: {
-        rejectUnauthorized: false, // Yandex Cloud использует самоподписанный сертификат
-        ca: [
-          require("fs").readFileSync(
-            join(__dirname, "..", ".postgresql", "root.crt"),
-            "utf-8"
-          ),
-        ],
+        rejectUnauthorized: false, // Yandex Cloud требует SSL но может использовать самоподписанный сертификат
       },
     }),
     TypeOrmModule.forFeature([UsersAndMonsters, CompetitionsInstancesMonsters]),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, MonstertypesController],
+  providers: [AppService, MonstertypesService],
 })
 export class AppModule {}
